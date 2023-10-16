e77e0db64af7dcf001f052c8a3014237c519cd75
root <root@LAPTOP-L2B-HA>
2023-10-16 17:31:26 +0000

    matomo*

--- codebase/src/Controller/Admin/DemandeController.php
+++ codebase/src/Controller/Admin/DemandeController.php
@@ -16,6 +16,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('admin/demande')]
 class DemandeController extends AbstractController
